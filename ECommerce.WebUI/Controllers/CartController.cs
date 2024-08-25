4b7b30b193cfe62953e2703fad1f02d2f95af03b
Elvin <camalzade_elvin@mail.ru>
2024-08-25 11:14:13 +0400

    done without auth

--- ECommerce.WebUI/Controllers/CartController.cs
+++ ECommerce.WebUI/Controllers/CartController.cs
@@ -1,4 +1,6 @@
 ﻿using ECommerce.Business.Abstract;
+using ECommerce.Entities.Concrete;
+using ECommerce.WebUI.Models;
 using ECommerce.WebUI.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,5 +30,45 @@ namespace ECommerce.WebUI.Controllers
             TempData.Add("message", String.Format("Your Product, {0} was added successfully to cart", productToBeAdded.ProductName));
             return RedirectToAction("Index","Product",new {page=page,category=category});
         }
+
+        public IActionResult List()
+        {
+            var cart=_cartSessionService.GetCart();
+            var model = new CartListViewModel
+            {
+                Cart= cart
+            };
+            return View(model);
+        }
+
+        public IActionResult Remove(int productId) { 
+            var cart=_cartSessionService.GetCart();
+            _cartService.RemoveFromCart(cart,productId);
+            _cartSessionService.SetCart(cart);
+            TempData.Add("message", "Your product removed successfully from cart");
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Complete()
+        {
+            var shippingDetailViewModel = new ShippingDetailViewModel
+            {
+                ShippingDetails = new ShippingDetails()
+            };
+            return View(shippingDetailViewModel);
+        }
+
+
+        [HttpPost]
+        public IActionResult Complete(ShippingDetailViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            TempData.Add("message", String.Format("Thank you {0} , you order is in progress", model.ShippingDetails.Firstname));
+            return RedirectToAction("List");
+        }
+
     }
 }
